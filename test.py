cf0147cb9bb50525a068415e8641ee6637c058da
exeption <exeption@MacBook-Air-2.local>
2024-12-13 00:28:34 +0000

    premier code

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 df = pd.DataFrame({
     'nom': ['arthur', 'matthias', 'theo'],
-    'age': [28, 22, 26]
+    'age': [28, 20, 26]
 })
 
-pd.to_csv(df)
+df.to_csv('dataframe.csv')
 
 print(df)
